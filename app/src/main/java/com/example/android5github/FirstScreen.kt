3ee429749ec469bf84ca3e5579fbc2b2bfcda182
Radin Tapchylov <vesolyyradin@gmail.com>
2023-11-17 19:37:23 +0600

    Added preview for this screen

--- app/src/main/java/com/example/android5github/FirstScreen.kt
+++ app/src/main/java/com/example/android5github/FirstScreen.kt
@@ -1,12 +1,31 @@
 package com.example.android5github
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.android5github.ui.theme.Android5GitHubTheme
 
 @Composable
 fun FirstScreen(modifier: Modifier = Modifier) {
     Box(modifier = modifier) {
 
+    }
+}
+
+@Preview
+@Composable
+fun PreviewFirstScreen() {
+    Android5GitHubTheme {
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            FirstScreen()
+        }
     }
 }
