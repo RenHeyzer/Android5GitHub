e29313a993eafcbccd08711db031fdb825ed0fca
Radin Tapchylov <vesolyyradin@gmail.com>
2023-11-17 19:41:38 +0600

    removed unused import

--- app/src/main/java/com/example/android5github/FirstScreen.kt
+++ app/src/main/java/com/example/android5github/FirstScreen.kt
@@ -6,7 +6,6 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.android5github.ui.theme.Android5GitHubTheme
 
