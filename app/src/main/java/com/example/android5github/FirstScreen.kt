f4bef4677644baef211a0d95abc10f573a03078b
Radin Tapchylov <vesolyyradin@gmail.com>
2023-11-21 18:42:21 +0600

    added imports

--- app/src/main/java/com/example/android5github/FirstScreen.kt
+++ app/src/main/java/com/example/android5github/FirstScreen.kt
@@ -2,8 +2,10 @@ package com.example.android5github
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
